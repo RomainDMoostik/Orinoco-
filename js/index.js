1216e7983677ea55031625e36b006e40c7fe91d3
Romain <romaindigue@gmail.com>
2020-10-26 09:38:14 +0100

    Mentorat

--- js/index.js
+++ js/index.js
@@ -1,45 +1,81 @@
 console.log('index');
-
+console.log(getTeddies());
 //const rowProduit = document.querySelector(".container > .my-4");
 
-/* */
-const containerProduit = document.createElement("div");
-containerProduit.classList.add("container", "my-4");
+//let teddiesTitle = getTeddies.name;
+/*
+getProducts();
 
-const rowProduit = document.createElement("div");
-rowProduit.classList.add("row", "my-4");
+async function getProducts() {
+  const productsList = [...await getTeddies(), ...await getBirds()]; // response.json // fetch
 
-const colProduit = document.createElement("div");
-colProduit.classList.add("col-12", "col-md-4", "my-3", "mx-auto");
+  for (const product of productsList) {
+    addCard(product);
+  }
+}
 
-const ficheProduit = document.createElement("div");
-ficheProduit.classList.add("card", "rounded-lg");
+getTeddies()
+.then((teddies) => {
+  return getBirds()
+  .then((birds) => {
+    const productsList = [...teddies, ...birds];
 
-const lienProduit = document.createElement("a");
-lienProduit.classList.add("stretched-link");
-lienProduit.setAttribute("href", "produit.html");
+    for (const product of productsList) {
+      addCard(product);
+    }
+  })
+})
 
-const corpsProduit = document.createElement("div");
-corpsProduit.classList.add("card-body");
+Promise.all([
+  getBirds(),
+  getTeddies(),
+]).then((products) => {
+  const [birds, teddies] = products;
+  
+})
 
-const imgProduit = document.createElement("img");
-imgProduit.classList.add("card-img-top");
-imgProduit.setAttribute("src", "#");
-imgProduit.setAttribute("alt", "#");
+ */
 
-const nomProduit = document.createElement("div");
-nomProduit.classList.add("card-title", "h5");
 
-const prixProduit = document.createElement("div");
-prixProduit.classList.add("card-text", "h6", "text-center");
+function addCard(product) {
+    const containerProduit = document.createElement("div");
+    containerProduit.classList.add("container", "my-4");
 
-rowProduit.appendChild(containerProduit);
-colProduit.appendChild(rowProduit),
-ficheProduit.appendChild(colProduit);
-nomProduit.appendChild(ficheProduit);
-corpsProduit.appendChild(ficheProduit);
-imgProduit.appendChild(ficheProduit);
-lienProduit.appendChild(ficheProduit);
+    const rowProduit = document.createElement("div");
+    rowProduit.classList.add("row", "my-4");
 
-containerProduit.textContent="<p>Test Container</>";
+    const colProduit = document.createElement("div");
+    colProduit.classList.add("col-12", "col-md-4", "my-3", "mx-auto");
+
+    const ficheProduit = document.createElement("div");
+    ficheProduit.classList.add("card", "rounded-lg");
+
+    const lienProduit = document.createElement("a");
+    lienProduit.classList.add("stretched-link");
+    lienProduit.setAttribute("href", "produit.html");
+
+    const corpsProduit = document.createElement("div");
+    corpsProduit.classList.add("card-body");
+
+    const imgProduit = document.createElement("img");
+    imgProduit.classList.add("card-img-top");
+    imgProduit.setAttribute("src", "#");
+    imgProduit.setAttribute("alt", "#");
+
+    const nomProduit = document.createElement("div");
+    nomProduit.classList.add("card-title", "h5"); // product.title
+
+    const prixProduit = document.createElement("div");
+    prixProduit.classList.add("card-text", "h6", "text-center");
+
+    rowProduit.appendChild(containerProduit);
+    colProduit.appendChild(rowProduit),
+    ficheProduit.appendChild(colProduit);
+    nomProduit.appendChild(ficheProduit);
+    corpsProduit.appendChild(ficheProduit);
+    imgProduit.appendChild(ficheProduit);
+    lienProduit.appendChild(ficheProduit);
+
+    containerProduit.textContent="<p>Test Container</>"; // product.description
+}
 
